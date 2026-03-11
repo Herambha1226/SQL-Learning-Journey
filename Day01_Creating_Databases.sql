35cd538675bf7a84a2f39bfef7b19d639fc8cce4
Herambha1226 <pallapothugayathri23@gmail.com>
2026-03-11 17:08:19 +0530

    Learning the Database 4 Operation

---
+++ Day01_Creating_Databases.sql
@@ -0,0 +1,16 @@
+# Create Databases 
+
+#create database karthikeya;
+create database Herambha;
+create database guptha;
+
+# Showing Databases
+show databases;
+
+# Using Database
+use herambha;
+
+# Drop or Delete the Database
+drop database karthikeya;
+drop database guptha;
+drop database herambha;
