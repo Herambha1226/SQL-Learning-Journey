d07bcc76a583d73ab0657aa048445b7d7e353ad0
Herambha1226 <pallapothugayathri23@gmail.com>
2026-03-11 18:10:57 +0530

    Learing CRUD Operations on Table

---
+++ Day04_Working_CRUD_Operations.sql
@@ -0,0 +1,38 @@
+# Below are the showing CRUD operations
+
+# Using Database
+use herambha;
+
+# C -> Create
+# Create Table 
+create table people_informatiom(
+id int primary key,
+full_name varchar(50) not null,
+email varchar(50) not null default 'example@gmail.com',
+age int,
+city varchar(50) default 'uppugundur'
+);
+
+# Inserting Data to the table
+insert into people_information values(1,'Herambha','herambhaa@gmail.com',18,'uppugundur');
+
+insert into people_information (id,full_name,age,city)
+values(2,'Karthikeya',18,'gunturu');
+
+insert into people_information (id,full_name,email,age)
+values(3,'Guptha',"hello@example.com",19);
+
+insert into people_information (id,full_name,age)
+values(4,'Eswara Rao',19);
+
+# R -> Reading which is key using (select)
+
+# Seeing the inside data of a table 
+select * from people_information;   
+
+# Showing Particular columns 
+select id,age from people_information;
+select id,full_name,email from people_information;
+
+
+
