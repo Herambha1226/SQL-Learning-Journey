bdb961d7d2cc956c8fb9bc47ba15157e62d9401c
Herambha1226 <pallapothugayathri23@gmail.com>
2026-03-11 17:38:10 +0530

    Learning Table Creating and Updating Columns

---
+++ Day02_Working_On_Tables.sql
@@ -0,0 +1,42 @@
+# Database Creating
+create database herambha;
+
+# Using herambha Database 
+use herambha;
+
+# Creating Table
+/* example -> 1 */
+create table student(
+id int ,
+name varchar(50)
+);
+
+/* example -> 2 */
+create table information(
+id int primary key,
+name varchar(50),
+age int,
+phone_number varchar(20),
+email varchar(50) unique
+);
+
+# Describe The Table ( it shows the structure of the table)
+describe information;
+
+# Alter is method modify or update or remove column and column type
+alter table herambha.information 
+add gender char(1);
+
+alter table herambha.information
+add city varchar(50);
+
+# Rename Column Name 
+alter table herambha.information
+rename column name to full_name;
+
+# Showing Tables which are having in database
+show tables from herambha;
+
+# Drop The Table From Database
+drop table student;
+
